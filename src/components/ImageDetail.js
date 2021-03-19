77922467b9fe3ea885b9b959608a1194bb769900
CristiMark-PC\Cristi Mark <cristiankeisz@hotmail.com>
2021-03-19 08:56:17 +0200

    Images

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { Text, StyleSheet, View, Button,TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, Button,TouchableOpacity, Image } from "react-native";
 
 const ImageDetail = (props) => {
-  return(
-   
+  return(       
+   <View>
      <Text style={styles.text}>{props.title}</Text>
-     
+     <Image source={props.imageSource}/>
+     <Text style={styles.score}>Score: {props.score}</Text>
+     </View>
      );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
   },
-  buttons:{
-   paddingTop: '3'
-  }
+  score: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  buttons: {
+    paddingTop: "3",
+  },
 });
 
 export default ImageDetail;
--- src/screens/imageScrre.js
+++ src/screens/imageScrre.js
@@ -1,25 +1,46 @@
 import React from "react";
-import { Text, StyleSheet, View, Button,TouchableOpacity } from "react-native";
-import ImageDetail from '../components/ImageDetail'
+import {
+  Text,
+  StyleSheet,
+  View,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import ImageDetail from "../components/ImageDetail";
+
 
 const ImageScreen = () => {
-  return(
-    <View>
-      <ImageDetail title ="Forest"/>
-      <ImageDetail title ="Beach" />
-      <ImageDetail title = "Mountain"/>
-    </View>
-
-     );
+  return (
+    <ScrollView>
+      <View>
+        <ImageDetail
+          title="Forest"
+          imageSource={require("../../assets/forest.jpg")}
+          score={5}
+        />
+        <ImageDetail
+          title="Beach"
+          imageSource={require("../../assets/beach.jpg")}
+          score={3}
+        />
+        <ImageDetail
+          title="Mountain"
+          imageSource={require("../../assets/mountain.jpg")}
+          score={7}
+        />
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
   },
-  buttons:{
-   paddingTop: '3'
-  }
+  buttons: {
+    paddingTop: "3",
+  },
 });
 
 export default ImageScreen;
